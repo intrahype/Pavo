3782ebce2e9b42588a159fa7c982767682146778
Kenyon Fryman <54225943+intrahype@users.noreply.github.com>
2024-06-22 01:15:24 -0700

    added first code

--- actions.py
+++ actions.py
@@ -1,8 +1,20 @@
 #import PIL and Pillow Libraries
-from PIL import Image
-import Pillow
-
 #import human-readable exif tags
 
-#function to iterate through photos and pull metadata
+from PIL import Image as PillowImage
+from PIL import ExifTags
 
+#function to iterate through photos and pull metadata
+for img in images:
+    print(img)
+    image_path = "unaltered/{}".format(img)
+    pillow_img = PillowImage.open(image_path)
+    img_exif = pillow_img.getexif()
+    
+    for tag in PILLOW_TAGS:
+        try:
+            english_tag = ExifTags.TAGS[tag]
+            value = img_exif[tag]
+        except:
+            continue
+        print("{}: {}".format(english_tag, value))
