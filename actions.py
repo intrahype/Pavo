8420a08e66f9d48bc8b31879b1f07d9502de4ddb
Kenyon Fryman <54225943+intrahype@users.noreply.github.com>
2024-07-09 15:56:27 -0700

    updated extraction method

--- actions.py
+++ actions.py
@@ -5,30 +5,22 @@ from PIL import Image as PillowImage
 from PIL.Exiftags import TAGS
 
 #create empty dict for images to populate
-images = []
+unmodified = [
 
-#add function to add images to dict
+ ]
 
-PILLOW_TAGS = [
+for file in unmodified:
+    #open image
+    image = image.open()
 
-]
+    #get exif data
+    exifdata = image.getexif()
 
-EXIF_TAGS = [
-    
-]
-
-#function to iterate through photos and pull metadata
-for img in images:
-    print(img)
-    image_path = "unaltered/{}".format(img)
-    pillow_img = PillowImage.open(image_path)
-    img_exif = pillow_img.getexif()
-    
-    for tag in PILLOW_TAGS:
-        try:
-            english_tag = ExifTags.TAGS[tag]
-            value = img_exif[tag]
-        except:
-            continue
-        #add code here to take exif data for input form
-        print("{}: {}".format(english_tag, value))
+    #loop to make tags human readable
+    for tagid in exifdata:
+        #get tagid
+        tagname = TAGS.get(tagid, tagid)
+        #get tag value
+        value = exifdata.get(tagid)
+        #print final result
+        print(f"{tagname:30}: {value}")
